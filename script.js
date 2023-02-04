8620c1c078b874f0c8580cb823700ac891865445
Sumit112207 <107878491+Sumit112207@users.noreply.github.com>
2023-02-04 23:03:41 +0530

    Add files via upload
    This is JS file Javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function handleSubmit(event) {
+	event.preventDefault();
+	let text = document.getElementById("inptxt").value;
+	let qrsrc =
+		`http://api.qrserver.com/v1/create-qr-code/?data=${text}`;
+	let qrimg = document.getElementById("qrimg");
+	qrimg.src = qrsrc;
+}
